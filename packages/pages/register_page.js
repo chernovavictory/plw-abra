3187af182976c9f7974bc640282d01f170a468c6
chernovavictory <chernovavi.qa@gmail.com>
2024-07-27 16:26:33 +0500

    registration test with valid data

--- packages/pages/register_page.js
+++ packages/pages/register_page.js
@@ -1,9 +1,10 @@
+const timeOut = 200;
 exports.RegistrationPage = class RegistrationPage {
     constructor(page) {
         this.page = page;
-        this.role_toggle_seller = page.locator('//*[@id="root"]/div/div/div/form/div[1]/button[2]');
         this.email_field = page.locator('//*[@id="root"]/div/div/div/form/div[2]/input');
         this.password_field = page.locator('//*[@id="root"]/div/div/div/form/div[3]/input');
+        this.submit_btn = page.locator('//*[@id="root"]/div/div/div/form/button');
     }
 
     async open_register_page() {
@@ -11,17 +12,20 @@ exports.RegistrationPage = class RegistrationPage {
         await this.page.waitForSelector('#root > div > div > div > form > div.RegisterForm_buttons_container_user_role__odHqW > button.Button_button__-\\+nph.Button_white__PFVOl.RegisterForm_button_user_role__ax6bX');
     }
 
-    async choose_seller_form() {
-        this.role_toggle_seller.click();
-    }
-
     async fill_email_field() {
         this.email_field.focus();
-        this.email_field.fill('jonive7823@hostlace.com');
+        this.email_field.fill('123@gmail.com');
+        await this.page.waitForTimeout(timeOut);
     }
 
     async fill_password_field() {
         this.password_field.focus();
         this.password_field.fill('Password1!')
+        await this.page.waitForTimeout(timeOut);
+    }
+
+    async click_submit_btn() {
+        this.submit_btn.click();
+        await this.page.waitForTimeout(1000);
     }
 }
--- packages/tests/register.test.js
+++ packages/tests/register.test.js
@@ -5,12 +5,11 @@ test('registration - valid data', async ({page}) => {
     const Registration = new RegistrationPage(page);
 
     await Registration.open_register_page();
-    await Registration.choose_seller_form();
-    await page.waitForTimeout(2000);
     await Registration.fill_email_field();
-    await page.waitForTimeout(2000);
     await Registration.fill_password_field();
-    await page.waitForTimeout(2000);
-    // await expect(Registration.email_field).toContainText('123@gmail.com');
+    await Registration.click_submit_btn();
 
-})
+    await expect(Registration.email_field).not.toBeVisible();
+});
+
+test('registration - invalid data')
